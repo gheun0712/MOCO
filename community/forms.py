a264fdff43bc6e5af6a249d42180af8f2b57944c
gheun712 <gheun071212@gmail.com>
2022-05-25 09:39:27 +0900

    0525 community front(forms.py)

--- community/forms.py
+++ community/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from matplotlib import widgets
 from .models import Review, ReviewComment
 
 
@@ -7,6 +8,31 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['title', 'movie_title', 'rank', 'content']
+        widgets = {
+            'title' : forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 400px;',
+                'placeholder': '글 제목을 입력해주세요.',
+            }),
+            
+            'movie_title' : forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 400px;',
+                'placeholder': '영화 제목을 입력해주세요.',
+            }),
+            
+            'rank' : forms.NumberInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 400px',
+                'placeholder': '평점을 입력해주세요.',
+            }),
+            
+            'content' : forms.Textarea(attrs={
+                'class': "form-control",
+                'style': 'max-width: 400px',
+                'placeholder': '내용을 입력해주세요.',
+            })
+        }
 
 
 class ReviewCommentForm(forms.ModelForm):
@@ -14,3 +40,4 @@ class ReviewCommentForm(forms.ModelForm):
     class Meta:
         model = ReviewComment
         exclude = ['review', 'user']
+        
